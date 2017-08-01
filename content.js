e797e3c4f3a75df9886374a54b0bd9e3e5e52dee
Dave Charnuska <dcharnuska@icapitalnetwork.com>
2017-07-31 23:05:31 -0400

    improve main page functionality, begin implementing user page functionality

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var clickedElement;
+let clickedImg;
 
 function isElement(el) {
   return el && el instanceof HTMLElement;
@@ -24,7 +24,7 @@ function isImg(el) {
   return el && el.tagName === "IMG";
 }
 
-function getMainImgSrc(el) {
+function getMainImg(el) {
   let parentNode = el.parentElement,
       childNode = parentNode.firstChild,
       grandchildNode = childNode.firstChild;
@@ -32,35 +32,43 @@ function getMainImgSrc(el) {
   if (isLink(el) || isLink(parentNode) || isLink(childNode)) {
     return null;
   } else if (isImg(grandchildNode)) {
-    return grandchildNode.getAttribute("src");
+    return grandchildNode;
   }
 
   return null;
 }
 
 function getUserPageImgSrc(el) {
+  let parentNode = el.parentElement,
+      childNode = parentNode.firstChild,
+      grandchildNode = childNode.firstChild,
+      greatGrandchildNode = grandchildNode.firstChild;
 
+  if (isImg(greatGrandchildNode)) {
+    return greatGrandchildNode.getAttribute("src");
+  }
+
+  return null;
 }
 
 document.addEventListener("mousedown", function(event){
   clickedEl = event.target;
-  var x = getMainImgSrc(clickedEl);
   // console.log(clickedElement.parentElement.firstChild.firstChild.getAttribute("src"))
   // let x = getImgSrc(clickedElement.parentElement.firstChild.firstChild);
   // console.log(x)
 
   if (document.location.pathname.split("/").length % 2 === 0) {
-    getMainImgSrc(clickedEl);
+    clickedImg = getMainImg(clickedEl);
   } else {
-    getUserPageImgSrc(clickedEl);
+    var x = getUserPageImgSrc(clickedEl);
+    console.log(x);
   }
 }, true);
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    let img = $(clickedElement).siblings().children()[0],
-        imageSrc = $(img).attr("src");
-
-    if (imageSrc) {
+    if (clickedImg) {
+      let imageSrc = clickedImg.getAttribute("src");
+      
       chrome.runtime.sendMessage({"message": "get_img_src", "src": imageSrc});
     }
 });
