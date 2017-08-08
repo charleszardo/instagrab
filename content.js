90e5176a7d91087c92e32b2f5e4e5cb8a5804574
Dave Charnuska <dcharnuska@icapitalnetwork.com>
2017-08-07 23:13:49 -0400

    add AJAX recognition functionality

--- content.js
+++ content.js
@@ -58,3 +58,39 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       chrome.runtime.sendMessage({"message": "get_img_src", "src": imageSrc});
     }
 });
+
+// experimental area
+
+// recognize AJAX requests:
+function injectScript(source) {
+
+    var elem = document.createElement("script"); //Create a new script element
+    elem.type = "text/javascript"; //It's javascript
+    elem.innerHTML = source; //Assign the source
+    document.documentElement.appendChild(elem); //Inject it into the DOM
+}
+
+injectScript("("+(function() {
+
+    function bindResponse(request, response) {
+        request.__defineGetter__("responseText", function() {
+            console.warn('Something tried to get the responseText');
+            console.debug(response);
+            return response;
+        })
+    }
+
+    function processResponse(request,caller,method,path) {
+        bindResponse(request, request.responseText);
+    }
+
+    var proxied = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function(method, path, async) {
+            var caller = arguments.callee.caller;
+            this.addEventListener('readystatechange', function() {
+                if (this.readyState === 4)
+                    processResponse(this,caller,method,path);
+            }, true);
+        return proxied.apply(this, [].slice.call(arguments));
+    };
+}).toString()+")()");
