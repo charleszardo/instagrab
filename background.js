b122353e5af50969256dbd08924d0176ba3dda7a
Dave Charnuska <dcharnuska@icapitalnetwork.com>
2017-09-11 23:16:44 -0400

    WIP

--- background.js
+++ background.js
@@ -1,15 +1,28 @@
 var parent = chrome.contextMenus.create({
   "title": "Instagrab",
-  "contexts": ["all"],
-  "documentUrlPatterns": ["*://*.instagram.com/*"]
+  "contexts": ["page"],
+  "documentUrlPatterns": ["*://*.instagram.com/*"],
+  "onclick": testFnc
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+
+function testFnc(info, tab) {
   chrome.tabs.sendMessage(tab.id, {});
-})
+}
+// chrome.contextMenus.onClicked.addListener(function (info, tab) {
+//   console.log('about to send msg');
+//   console.log(info);
+//   console.log(tab);
+//   chrome.tabs.sendMessage(tab.id, {});
+//   console.log('msg sent');
+// })
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    console.log('msg received');
+    console.log(request);
+    console.log(sender);
+    console.log(sendResponse);
     if( request.message === "get_img_src" ) {
       chrome.downloads.download({url: request.src});
     }
--- content.js
+++ content.js
@@ -58,3 +58,62 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       chrome.runtime.sendMessage({"message": "get_img_src", "src": imageSrc});
     }
 });
+
+// experimental area
+
+// recognize AJAX requests:
+function injectScript(source) {
+
+    var elem = document.createElement("script"); //Create a new script element
+    elem.type = "text/javascript"; //It's javascript
+    elem.innerHTML = source; //Assign the source
+    document.documentElement.appendChild(elem); //Inject it into the DOM
+}
+
+injectScript("("+(function() {
+
+    function bindResponse(request, response) {
+        request.__defineGetter__("responseText", function() {
+            console.warn('Something tried to get the responseText');
+            console.debug(response);
+            return response;
+        })
+    }
+
+    function processResponse(request,caller,method,path) {
+        bindResponse(request, request.responseText);
+    }
+
+    var proxied = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function(method, path, async) {
+            var caller = arguments.callee.caller;
+            this.addEventListener('readystatechange', function() {
+                if (this.readyState === 4)
+                    processResponse(this,caller,method,path);
+            }, true);
+        return proxied.apply(this, [].slice.call(arguments));
+    };
+}).toString()+")()");
+
+// begin developing mouseover approach
+
+// get all images
+let imgList = [...document.querySelectorAll("img")];
+console.log(imgList)
+
+// filter out profile images
+imgList.filter(img => img.width > 30);
+
+// get class name from first image since all have same class/markup
+// get DOM node to start traversing
+let imgNode = document.querySelector(`.${imgList[0].className}`);
+
+// masking element is a sibling of the node
+let siblingNode = imgNode.parentElement.nextElementSibling;
+
+// once class name of sibling node is determined it should be saved somewhere
+// so that above work doesn't have to be repeated, slowing down the page
+
+// attach mouseover to all elements with siblingNode class
+
+// attach mouseover event every time and AJAX event occurs (unless better approach is developed)
